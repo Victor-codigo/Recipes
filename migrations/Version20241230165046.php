5d88af74b8e7982eec6d71694a714638b9fb448e
Víctor <{GIT_EMAIL}>
2025-01-07 10:46:54 +0100

    feat: :sparkles: Update migration descriptions for user, messenger_messages, and recipe tables

--- migrations/Version20241230165046.php
+++ migrations/Version20241230165046.php
@@ -7,14 +7,11 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20241230165046 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Creates table user and messenger_messages';
     }
 
     public function up(Schema $schema): void
--- migrations/Version20241230165103.php
+++ migrations/Version20241230165103.php
@@ -7,14 +7,11 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20241230165103 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Creates table recipe';
     }
 
     public function up(Schema $schema): void
