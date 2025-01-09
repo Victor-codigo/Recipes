3277b083af78790439772aab0cc0bbf2176db54c
Víctor <{GIT_EMAIL}>
2025-01-09 16:55:46 +0100

    feat: :sparkles: Register Alert, Modal, Paginator, and List components in bootstrap.js and update base.html.twig for asset management

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -6,12 +6,37 @@ export const app = startStimulusApp(require.context(
     /\.[jt]sx?$/
 ));
 
-// HOME LIST
+// ALERT COMPONENT
+import AlertValidationComponent from 'App/Twig/Components/Alert/AlertComponent_controller.js';
+
+// MODAL COMPONENT
+import ModalComponent from 'App/Twig/Components/Modal/Modal_Component.js';
+
+// PAGINATION COMPONENT
+import PaginatorComponent from 'App/Twig/Components/Paginator/Paginator_Component.js';
+
+// LIST COMPONENT
+import ListComponent from 'App/Twig/Components/List/List_controller.js';
+
+// HOME LIST COMPONENT
 import HomeSectionComponent from 'App/Twig/Components/HomeSection/Home/HomeSection_controller';
 import HomeListComponent from 'App/Twig/Components/HomeSection/HomeList/List/HomeList_controller';
 import HomeListItemComponent from 'App/Twig/Components/HomeSection/HomeList/ListItem/HomeListItem_controller';
 
-// HOME LIST
+
+// ALERT COMPONENT
+app.register('AlertValidationComponent', AlertValidationComponent);
+
+// MODAL COMPONENT
+app.register('ModalComponent', ModalComponent);
+
+// PAGINATION COMPONENT
+app.register('PaginatorComponent', PaginatorComponent);
+
+// LIST COMPONENT
+app.register('ListComponent', ListComponent);
+
+// HOME LIST COMPONENT
 app.register('HomeSectionComponent', HomeSectionComponent);
 app.register('HomeListComponent', HomeListComponent);
 app.register('HomeListItemComponent', HomeListItemComponent);
