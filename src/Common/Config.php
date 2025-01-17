25f5f0129b0f923f966e6871cc0f9fd3ebeff99d
Víctor <{GIT_EMAIL}>
2025-01-17 13:32:33 +0100

    feat: :sparkles: Add constant for maximum pagination items

--- src/Common/Config.php
+++ src/Common/Config.php
@@ -7,4 +7,6 @@ namespace App\Common;
 class Config
 {
     public const string RECIPE_IMAGE_NO_IMAGE_PUBLIC_PATH = 'images/recipes/recipe-no-image.svg';
+
+    public const int PAGINATION_PAGE_MAX_ITEMS = 100;
 }
