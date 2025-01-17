19c1f800c6d9e7dddceaf54ae9ff737fea6f8164
Víctor <{GIT_EMAIL}>
2025-01-17 13:55:21 +0100

    refactor: :bug: Fixed class ModalComponent it contains ModalComponentDto instead

--- templates/Components/Modal/ModalComponent.php
+++ templates/Components/Modal/ModalComponent.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace App\Templates\Components\Modal;
 
+use App\Templates\Components\TwigComponent;
 use App\Templates\Components\TwigComponentDtoInterface;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-class ModalComponentDto implements TwigComponentDtoInterface
+#[AsTwigComponent(
+    name: 'ModalComponent',
+    template: 'Components/Modal/ModalComponent.php'
+)]
+class ModalComponent extends TwigComponent
 {
-    /**
-     * @param ModalComponentButtonDto[] $buttons
-     * */
-    public function __construct(
-        public readonly string $idAttribute,
-        public readonly string $title,
-        public readonly bool $closeButton,
-        public readonly ?string $contentComponentName,
-        public readonly string|TwigComponentDtoInterface $content,
+    public ModalComponentDto&TwigComponentDtoInterface $data;
 
-        public readonly array $buttons,
-    ) {
+    public static function getComponentName(): string
+    {
+        return 'ModalComponent';
+    }
+
+    public function mount(ModalComponentDto&TwigComponentDtoInterface $data): void
+    {
+        $this->data = $data;
     }
 }
