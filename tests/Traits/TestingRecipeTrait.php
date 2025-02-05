552496429f7c2b8c4ea484573e800e9b1255d54f
Víctor <{GIT_EMAIL}>
2025-02-05 18:31:05 +0100

    refactor: :bug: Fixed a bug in method TestingRecipeTrait::createRecipeFormDataValidation
    The class RecipeCreateFormDataValidation no longer pass parameters by contructor

--- tests/Traits/TestingRecipeTrait.php
+++ tests/Traits/TestingRecipeTrait.php
@@ -202,25 +202,26 @@ trait TestingRecipeTrait
 
     protected function createRecipeFormDataValidation(): RecipeCreateFormDataValidation
     {
-        return new RecipeCreateFormDataValidation(
-            'Recipe name',
-            'Recipe description',
-            [
-                '2 potatoes',
-                '1 cucumber',
-                '1 onion',
-                '1 carrot',
-            ],
-            [
-                '1. Prepare the potatoes: Cut the potatoes into bite-sized cubes.',
-                '2. Add the cucumbers, onion, and carrot to a large pot.',
-                '3. Cook the vegetables according to package instructions.',
-                '4. Add the potatoes to the cooked vegetables and cook for 30 minutes.',
-            ],
-            null,
-            new \DateTimeImmutable('2025-02-01 12:00:00'),
-            RECIPE_TYPE::BREAKFAST,
-            false
-        );
+        $recipeFormValidationData = new RecipeCreateFormDataValidation();
+        $recipeFormValidationData->name = 'Recipe name';
+        $recipeFormValidationData->description = 'Recipe description';
+        $recipeFormValidationData->ingredients = [
+            '2 potatoes',
+            '1 cucumber',
+            '1 onion',
+            '1 carrot',
+        ];
+        $recipeFormValidationData->steps = [
+            '1. Prepare the potatoes: Cut the potatoes into bite-sized cubes.',
+            '2. Add the cucumbers, onion, and carrot to a large pot.',
+            '3. Cook the vegetables according to package instructions.',
+            '4. Add the potatoes to the cooked vegetables and cook for 30 minutes.',
+        ];
+        $recipeFormValidationData->image = null;
+        $recipeFormValidationData->preparation_time = new \DateTimeImmutable('2025-02-01 12:00:00');
+        $recipeFormValidationData->category = RECIPE_TYPE::BREAKFAST;
+        $recipeFormValidationData->public = false;
+
+        return $recipeFormValidationData;
     }
 }
