264d03280139074546e5fd929f8e5a7340190e7a
Víctor <{GIT_EMAIL}>
2025-02-05 18:01:01 +0100

    feat: :sparkles: Add RecipeCreateController for handling recipe creation requests and form processing

---
+++ src/Controller/Recipe/RecipeCreate/RecipeCreateController.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Recipe\RecipeCreate;
+
+use App\Common\Config;
+use App\Controller\Exception\FormDataEmptyException;
+use App\Form\Factory\FormFactoryExtendedInterface;
+use App\Form\Factory\Form\FormTranslated;
+use App\Form\Recipe\RecipeCreate\RECIPE_CREATE_FORM_FIELDS;
+use App\Form\Recipe\RecipeCreate\RecipeCreateFormDataValidation;
+use App\Form\Recipe\RecipeCreate\RecipeCreateFormType;
+use App\Service\Recipe\RecipeCreate\RecipeCreateService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(
+    name: 'recipe_create',
+    path: '/{_locale}/recipe/create',
+    methods: ['POST'],
+    requirements: [
+        '_locale' => 'en|es',
+    ]
+)]
+class RecipeCreateController extends AbstractController
+{
+    public function __construct(
+        private RecipeCreateService $recipeCreateService,
+        private FormFactoryExtendedInterface $formFactoryExtended,
+    ) {
+    }
+
+    /**
+     * @throws FormDataEmptyException
+     */
+    public function __invoke(Request $request): Response
+    {
+        $form = $this->formFactoryExtended->createNamedTranslated(RECIPE_CREATE_FORM_FIELDS::FORM_NAME->value, RecipeCreateFormType::class, 'RecipeCreateComponent');
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->recipeCreate($form);
+        }
+
+        $this->AddFormErrorsToFlashBag($form);
+
+        return $this->redirectToRoute('recipe_home', [
+            'page' => 1,
+            'pageItems' => Config::PAGINATION_PAGE_MAX_ITEMS,
+        ]);
+    }
+
+    /**
+     * @param FormInterface<FormTranslated> $form
+     */
+    private function recipeCreate(FormInterface $form): void
+    {
+        /** @var RecipeCreateFormDataValidation */
+        $formData = $form->getData();
+
+        $this->recipeCreateService->__invoke($formData, null);
+    }
+
+    /**
+     * @param FormInterface<FormTranslated> $form
+     */
+    private function AddFormErrorsToFlashBag(FormInterface $form): void
+    {
+        foreach ($form->getErrors(true) as $error) {
+            $errorMessage = $error->getMessage();
+            $this->addFlash('error', $errorMessage);
+        }
+    }
+}
