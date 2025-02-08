184942bbee6ce2b14f41bbfee65536e8961924fb
Víctor <{GIT_EMAIL}>
2025-02-08 17:15:04 +0100

    refactor: :recycle: Update RecipeCreateFormType to enhance form field organization and improve success message handling

--- src/Form/Recipe/RecipeCreate/RecipeCreateFormType.php
+++ src/Form/Recipe/RecipeCreate/RecipeCreateFormType.php
@@ -12,11 +12,13 @@ use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\Dropzone\Form\DropzoneType;
 
@@ -43,22 +45,28 @@ class RecipeCreateFormType extends FormTypeBase implements FormTypeTranslatedInt
         parent::buildForm($builder, $options);
 
         $builder
+            // ->add(RECIPE_CREATE_FORM_FIELDS::CSRF_TOKEN->value, HiddenType::class)
             ->add(RECIPE_CREATE_FORM_FIELDS::NAME->value, TextType::class, ['trim' => true])
             ->add(RECIPE_CREATE_FORM_FIELDS::DESCRIPTION->value, TextareaType::class, ['trim' => true])
-            ->add(RECIPE_CREATE_FORM_FIELDS::INGREDIENTS->value, CollectionType::class, ['allow_add' => true])
-            ->add(RECIPE_CREATE_FORM_FIELDS::STEPS->value, CollectionType::class, ['allow_add' => true])
-            ->add(RECIPE_CREATE_FORM_FIELDS::IMAGE->value, DropzoneType::class)
             ->add(RECIPE_CREATE_FORM_FIELDS::PREPARATION_TIME->value, NumberType::class)
             ->add(RECIPE_CREATE_FORM_FIELDS::CATEGORY->value, EnumType::class, ['class' => RECIPE_TYPE::class])
             ->add(RECIPE_CREATE_FORM_FIELDS::PUBLIC->value, CheckboxType::class)
+            ->add(RECIPE_CREATE_FORM_FIELDS::INGREDIENTS->value, CollectionType::class, ['allow_add' => true])
+            ->add(RECIPE_CREATE_FORM_FIELDS::STEPS->value, CollectionType::class, ['allow_add' => true])
+            ->add(RECIPE_CREATE_FORM_FIELDS::IMAGE->value, DropzoneType::class)
             ->add(RECIPE_CREATE_FORM_FIELDS::SUBMIT->value, SubmitType::class);
     }
 
     /**
-     * @return Collection<int, string>
+     * @return Collection<array-key, FormError>
      */
     public function getFormSuccessMessages(): Collection
     {
-        return new ArrayCollection(RecipeCreateFormDataValidation::FORM_SUCCESS_MESSAGES);
+        $messagesOk = [];
+        foreach (RecipeCreateFormDataValidation::FORM_SUCCESS_MESSAGES as $message) {
+            $messagesOk[] = new FormError($message);
+        }
+
+        return new ArrayCollection($messagesOk);
     }
 }
