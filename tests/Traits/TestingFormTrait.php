861df06f8344377abaea8f4fc9a7e71d1d6c00a3
Víctor <{GIT_EMAIL}>
2025-03-11 13:37:28 +0100

    feat: :sparkles: Enhance TestingFormTrait with additional assertions for recipe file upload and modification validation

--- tests/Traits/TestingFormTrait.php
+++ tests/Traits/TestingFormTrait.php
@@ -5,14 +5,40 @@ declare(strict_types=1);
 namespace App\Tests\Traits;
 
 use App\Common\RECIPE_TYPE;
-use App\Controller\Recipe\RecipeCreate\RecipeCreateController;
 use App\Entity\Recipe;
 use App\Form\Recipe\RecipeCreate\RECIPE_CREATE_FORM_FIELDS;
+use App\Form\Recipe\RecipeModify\RECIPE_MODIFY_FORM_FIELDS;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Component\HttpFoundation\Session\Session;
 use VictorCodigo\SymfonyFormExtended\Form\FormMessage;
 
 trait TestingFormTrait
 {
+    protected function assertRecipeFileIsUploaded(Recipe $recipeExpected, string $uploadPath, ?string $recipeFormerImageFileName): void
+    {
+        self::assertNotNull($recipeExpected->getImage());
+        self::assertFileExists("{$uploadPath}/{$recipeExpected->getImage()}");
+
+        if (null !== $recipeFormerImageFileName) {
+            self::assertFileDoesNotExist("{$uploadPath}/{$recipeFormerImageFileName}");
+        }
+    }
+
+    protected function assertRecipeFileIsNotUploaded(Recipe $recipeExpected, string $uploadPath): void
+    {
+        self::assertNull($recipeExpected->getImage());
+        self::assertFileExists("{$uploadPath}/{$recipeExpected->getImage()}");
+    }
+
+    protected function assertRecipeFileIsRemoved(Recipe $recipeExpected, string $uploadPath, ?string $fileName): void
+    {
+        self::assertNull($recipeExpected->getImage());
+
+        if (null !== $fileName) {
+            self::assertFileDoesNotExist("{$uploadPath}/{$fileName}");
+        }
+    }
+
     /**
      * @param array{
      *      name: string,
@@ -24,36 +50,21 @@ trait TestingFormTrait
      *      ingredients: array<int, string>
      * } $formData
      */
-    protected function assertRecipeIsSavedInDataBaseAndRemoveImage(array $formData, string $uploadPath, bool $fileUploaded): void
+    protected function assertRecipeIsSavedInDataBase(Recipe $recipeExpected, array $formData): void
     {
-        /** @var Recipe|null */
-        $recipe = $this->recipeRepository->findOneBy(['name' => $formData[RECIPE_CREATE_FORM_FIELDS::NAME->value]]);
-
-        self::assertNotNull($recipe);
-        self::assertEquals($formData[RECIPE_CREATE_FORM_FIELDS::NAME->value], $recipe->getName());
-        self::assertEquals($formData[RECIPE_CREATE_FORM_FIELDS::DESCRIPTION->value] ?? null, $recipe->getDescription());
-        self::assertEquals(RECIPE_TYPE::tryFrom($formData[RECIPE_CREATE_FORM_FIELDS::CATEGORY->value]), $recipe->getCategory());
+        self::assertEquals($formData[RECIPE_CREATE_FORM_FIELDS::NAME->value], $recipeExpected->getName());
+        self::assertEquals($formData[RECIPE_CREATE_FORM_FIELDS::DESCRIPTION->value] ?? null, $recipeExpected->getDescription());
+        self::assertEquals(RECIPE_TYPE::tryFrom($formData[RECIPE_CREATE_FORM_FIELDS::CATEGORY->value]), $recipeExpected->getCategory());
 
         $preparationTime = null;
         if (isset($formData[RECIPE_CREATE_FORM_FIELDS::PREPARATION_TIME->value])) {
             $preparationTime = new \DateTimeImmutable('1970-01-01 '.$formData[RECIPE_CREATE_FORM_FIELDS::PREPARATION_TIME->value]);
         }
 
-        self::assertEquals($preparationTime, $recipe->getPreparationTime());
-        self::assertEquals($formData[RECIPE_CREATE_FORM_FIELDS::PUBLIC->value] ?? false, $recipe->getPublic());
-        self::assertEquals($formData[RECIPE_CREATE_FORM_FIELDS::INGREDIENTS->value], $recipe->getIngredients());
-        self::assertEquals($formData[RECIPE_CREATE_FORM_FIELDS::STEPS->value], $recipe->getSteps());
-
-        if ($fileUploaded) {
-            self::assertNotNull($recipe->getImage());
-            self::assertFileExists("{$uploadPath}/{$recipe->getImage()}");
-        }
-
-        if (!$fileUploaded) {
-            self::assertNull($recipe->getImage());
-        }
-
-        unlink("{$uploadPath}/{$recipe->getImage()}");
+        self::assertEquals($preparationTime, $recipeExpected->getPreparationTime());
+        self::assertEquals($formData[RECIPE_CREATE_FORM_FIELDS::PUBLIC->value] ?? false, $recipeExpected->getPublic());
+        self::assertEquals($formData[RECIPE_CREATE_FORM_FIELDS::INGREDIENTS->value], $recipeExpected->getIngredients());
+        self::assertEquals($formData[RECIPE_CREATE_FORM_FIELDS::STEPS->value], $recipeExpected->getSteps());
     }
 
     /**
@@ -74,14 +85,44 @@ trait TestingFormTrait
     }
 
     /**
+     * @param array{
+     *      id: string,
+     *      name: string,
+     *      description?: string|null,
+     *      preparation_time?: string|null,
+     *      category: string,
+     *      public?: bool,
+     *      steps: array<int, string>,
+     *      ingredients: array<int, string>
+     * } $formData
+     */
+    protected function assertRecipeIsModifiedInDataBase(Recipe $recipeExpected, array $formData): void
+    {
+        self::assertEquals($formData[RECIPE_MODIFY_FORM_FIELDS::ID->value], $recipeExpected->getId());
+        self::assertEquals($formData[RECIPE_MODIFY_FORM_FIELDS::NAME->value], $recipeExpected->getName());
+        self::assertEquals($formData[RECIPE_MODIFY_FORM_FIELDS::DESCRIPTION->value] ?? null, $recipeExpected->getDescription());
+        self::assertEquals(RECIPE_TYPE::tryFrom($formData[RECIPE_MODIFY_FORM_FIELDS::CATEGORY->value]), $recipeExpected->getCategory());
+
+        $preparationTime = null;
+        if (isset($formData[RECIPE_MODIFY_FORM_FIELDS::PREPARATION_TIME->value])) {
+            $preparationTime = new \DateTimeImmutable('1970-01-01 '.$formData[RECIPE_MODIFY_FORM_FIELDS::PREPARATION_TIME->value]);
+        }
+
+        self::assertEquals($preparationTime, $recipeExpected->getPreparationTime());
+        self::assertEquals($formData[RECIPE_MODIFY_FORM_FIELDS::PUBLIC->value] ?? false, $recipeExpected->getPublic());
+        self::assertEquals($formData[RECIPE_MODIFY_FORM_FIELDS::INGREDIENTS->value], $recipeExpected->getIngredients());
+        self::assertEquals($formData[RECIPE_MODIFY_FORM_FIELDS::STEPS->value], $recipeExpected->getSteps());
+    }
+
+    /**
      * @param array<int, string> $messagesOkExpected
      */
-    protected function assertResponseHasFlashMessageSuccess(array $messagesOkExpected): void
+    protected function assertResponseHasFlashMessageSuccess(string $flashBagMessageType, array $messagesOkExpected): void
     {
         /** @var Session */
         $session = $this->clientAuthenticated->getRequest()->getSession();
         /** @var array<int, FormMessage> */
-        $messagesOk = $session->getFlashBag()->get(RecipeCreateController::FORM_FLASH_BAG_MESSAGES_SUCCESS);
+        $messagesOk = $session->getFlashBag()->get($flashBagMessageType);
         $messagesText = array_map(
             fn (FormMessage $messageOk): string => $messageOk->template,
             $messagesOk
@@ -92,22 +133,22 @@ trait TestingFormTrait
         }
     }
 
-    protected function assertResponseHasNotFlashMessageSuccess(): void
+    protected function assertResponseHasNotFlashMessageSuccess(string $flashBagMessageType): void
     {
         /** @var Session */
         $session = $this->clientAuthenticated->getRequest()->getSession();
-        $this->assertEmpty($session->getFlashBag()->get(RecipeCreateController::FORM_FLASH_BAG_MESSAGES_SUCCESS));
+        $this->assertEmpty($session->getFlashBag()->get($flashBagMessageType));
     }
 
     /**
      * @param array<int, string> $messagesErrorExpected
      */
-    protected function assertResponseHasFlashMessageError(array $messagesErrorExpected): void
+    protected function assertResponseHasFlashMessageError(string $flashBagMessageType, array $messagesErrorExpected): void
     {
         /** @var Session */
         $session = $this->clientAuthenticated->getRequest()->getSession();
         /** @var array<int, FormMessage> */
-        $messagesError = $session->getFlashBag()->get(RecipeCreateController::FORM_FLASH_BAG_MESSAGES_ERROR);
+        $messagesError = $session->getFlashBag()->get($flashBagMessageType);
         $messagesText = array_map(
             fn (FormMessage $messageError): string => $messageError->template,
             $messagesError
@@ -118,19 +159,19 @@ trait TestingFormTrait
         }
     }
 
-    protected function assertResponseHasNotFlashMessageError(): void
+    protected function assertResponseHasNotFlashMessageError(string $flashBagMessageType): void
     {
         /** @var Session */
         $session = $this->clientAuthenticated->getRequest()->getSession();
-        $this->assertEmpty($session->getFlashBag()->get(RecipeCreateController::FORM_FLASH_BAG_MESSAGES_ERROR));
+        $this->assertEmpty($session->getFlashBag()->get($flashBagMessageType));
     }
 
     /**
      * @throws \Exception
      */
-    protected function getFormCsrfToken(string $formUrl, string $tokenSelector): string
+    protected function getFormCsrfToken(KernelBrowser $client, string $formUrl, string $tokenSelector): string
     {
-        $pageCrawler = $this->clientAuthenticated->request('GET', $formUrl);
+        $pageCrawler = $client->request('GET', $formUrl);
 
         $csrfToken = $pageCrawler
             ->filter($tokenSelector)
