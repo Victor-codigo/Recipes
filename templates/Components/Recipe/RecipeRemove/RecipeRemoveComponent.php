5b4b3b1b620eb67b43a630de0710cbaee6e6e6bf
Víctor <{GIT_EMAIL}>
2025-03-17 12:40:59 +0100

    fix: :bug: Update RecipeRemoveComponent to use correct form fields for single recipe removal

--- templates/Components/Recipe/RecipeRemove/RecipeRemoveComponent.php
+++ templates/Components/Recipe/RecipeRemove/RecipeRemoveComponent.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Templates\Components\Recipe\RecipeRemove;
 
-use App\Form\Recipe\RecipeRemoveMulti\RECIPE_REMOVE_MULTI_FORM_FIELDS;
 use App\Form\Recipe\RecipeRemove\RECIPE_REMOVE_FORM_FIELDS;
 use App\Templates\Components\HomeSection\ItemRemove\ItemRemoveComponent;
 use App\Templates\Components\HomeSection\ItemRemove\ItemRemoveComponentDto;
@@ -46,10 +45,10 @@ class RecipeRemoveComponent extends ItemRemoveComponent
     private function createRemoveMultiData(): array
     {
         return [
-            RECIPE_REMOVE_MULTI_FORM_FIELDS::FORM_NAME->value,
-            RECIPE_REMOVE_MULTI_FORM_FIELDS::getNameWithForm(RECIPE_REMOVE_MULTI_FORM_FIELDS::SUBMIT),
-            RECIPE_REMOVE_MULTI_FORM_FIELDS::getNameWithForm(RECIPE_REMOVE_MULTI_FORM_FIELDS::RECIPES_ID, true),
-            RECIPE_REMOVE_MULTI_FORM_FIELDS::getNameWithForm(RECIPE_REMOVE_MULTI_FORM_FIELDS::CSRF_TOKEN),
+            RECIPE_REMOVE_FORM_FIELDS::FORM_NAME->value,
+            RECIPE_REMOVE_FORM_FIELDS::getNameWithForm(RECIPE_REMOVE_FORM_FIELDS::SUBMIT),
+            RECIPE_REMOVE_FORM_FIELDS::getNameWithForm(RECIPE_REMOVE_FORM_FIELDS::RECIPES_ID, true),
+            RECIPE_REMOVE_FORM_FIELDS::getNameWithForm(RECIPE_REMOVE_FORM_FIELDS::CSRF_TOKEN),
             $this->translate('form.message_advice.text_multi'),
             $this->translate('form.buttons.close.label'),
             $this->translate('form.buttons.remove.label'),
